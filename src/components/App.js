581c9fd3b1e10de81a717bdb7676f43aac81b5a3
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-29 18:12:19 -0400

    Add Logo component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 
 import TextContainer from './Text/TextContainer';
+import Logo from './Page/Logo';
 import QuoteContainer from './Quote/QuoteContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Logo />
         <QuoteContainer />
         <TextContainer />
       </div>
---
+++ src/components/Page/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className='Logo'>
+      <h1>One Hundred Years of Lorem Ipsum</h1>
+    </div>
+  );
+};
+
+export default Logo;
