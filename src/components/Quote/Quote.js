c7b370481d317c9f45dcdee9cb97d6e63e8acdfa
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-30 13:36:13 -0400

    Add react-addons-css-transition-group

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 const QuoteWrapper = styled.div`
   font-size: 16px;
+
+  .example-enter {
+    opacity: 0.01;
+  }
+
+  .example-enter.example-enter-active {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+
+  .example-leave {
+    opacity: 1;
+  }
+
+  .example-leave.example-leave-active {
+    opacity: 0.01;
+    transition: opacity 300ms ease-in;
+  }
 `;
 
 const Quote = ({ quote }) => {
   return (
     <QuoteWrapper>
-      { quote }
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+      <h4>{ quote }</h4>
+      </ReactCSSTransitionGroup>
     </QuoteWrapper>
   );
 };
